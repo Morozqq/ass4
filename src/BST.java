d07de359e9b5f0909b3f723351f3827ac89fcfa9
Morozqq <aslan.dzheleubaj.04@gmail.com>
2023-05-11 18:43:02 +0600

    Delete section added getReplacementNode added when no children,when 1 child,when 2 children

--- src/BST.java
+++ src/BST.java
@@ -40,9 +40,86 @@ public class BST <K extends Comparable<K>, V>{
     public V get(K key){
 
     }
-    public void delete (K key){
+    public void delete(K key) {
+        Node focusNode = root;
+        Node parent = root;
+        boolean isLeftChild = true;
 
+        while (focusNode != null && focusNode.key.compareTo(key) != 0) {
+            parent = focusNode;
+            if (key.compareTo(focusNode.key) < 0) {
+                focusNode = focusNode.left;
+                isLeftChild = true;
+            } else {
+                focusNode = focusNode.right;
+                isLeftChild = false;
+            }
+        }
+
+        if (focusNode == null) {
+            return;
+        }
+
+
+        if (focusNode.left == null && focusNode.right == null) {
+            if (focusNode == root) {
+                root = null;
+            } else if (isLeftChild) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        }
+
+        else if (focusNode.left == null) {
+            if (focusNode == root) {
+                root = focusNode.right;
+            } else if (isLeftChild) {
+                parent.left = focusNode.right;
+            } else {
+                parent.right = focusNode.right;
+            }
+        } else if (focusNode.right == null) {
+            if (focusNode == root) {
+                root = focusNode.left;
+            } else if (isLeftChild) {
+                parent.left = focusNode.left;
+            } else {
+                parent.right = focusNode.left;
+            }
+        }
+
+        else {
+            Node replacement = getReplacementNode(focusNode);
+            if (focusNode == root) {
+                root = replacement;
+            } else if (isLeftChild) {
+                parent.left = replacement;
+            } else {
+                parent.right = replacement;
+            }
+            replacement.left = focusNode.left;
+        }
     }
+
+    private Node getReplacementNode(Node replacedNode) {
+        Node replacementParent = replacedNode;
+        Node replacement = replacedNode;
+        Node focusNode = replacedNode.right;
+        while (focusNode != null) {
+            replacementParent = replacement;
+            replacement = focusNode;
+            focusNode = focusNode.left;
+        }
+        if (replacement != replacedNode.right) {
+            replacementParent.left = replacement.right;
+            replacement.right = replacedNode.right;
+        }
+        return replacement;
+    }
+
+
+
     public Iterable<K> iterator(){
 
     }
