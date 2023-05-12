a8327f2739e7b6b47f5a9e25f0fa5d866d6774da
Morozqq <aslan.dzheleubaj.04@gmail.com>
2023-05-12 23:44:25 +0600

    Testcases

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BST<Integer, String> bst = new BST<>();
+
+        // Test putting nodes
+        bst.put(10, "Asik");
+        bst.put(20, "Basik");
+        bst.put(5, "Casic");
+        bst.put(15, "Dasic");
+        bst.put(3, "Esik");
+        bst.put(7, "Fesic");
+        bst.put(17, "Gesic");
+        bst.put(23, "Hesik");
+        bst.put(19, "Ibala");
+
+        // Test getting nodes
+        System.out.println(bst.get(10));
+        System.out.println(bst.get(20));
+        System.out.println(bst.get(5));
+        System.out.println(bst.get(15));
+
+
+        // Test deleting nodes
+        bst.delete(20);
+        bst.delete(3);
+        bst.delete(23);
+        System.out.println(bst.get(20));
+        System.out.println(bst.get(3));
+        System.out.println(bst.get(23));
+
     }
 }
