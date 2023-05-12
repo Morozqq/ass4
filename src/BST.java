258ee436ce94e88c5e0e6523c831819cf840c9b3
Morozqq <aslan.dzheleubaj.04@gmail.com>
2023-05-12 22:38:05 +0600

    updated the v get method and added some comments

--- src/BST.java
+++ src/BST.java
@@ -9,24 +9,31 @@ public class BST <K extends Comparable<K>, V>{
             this.val=val;
         }
     }
-
-    public void put(K key, V value) {
-        Node newNode = new Node(key, value);
+    //putting a node in the tree
+    public void put(K key, V val) {
+        // new node is created with the given key-value pair
+        Node newNode = new Node(key, val);
+        //if root is null the new node becomes a root
         if (root == null) {
             root = newNode;
         } else {
             Node focusNode = root;
             Node parent;
+            // iterate until a return statement is executed
             while (true) {
                 parent = focusNode;
+                // if key is less than the focusNode it goes to left
                 if (key.compareTo(focusNode.key) < 0) {
                     focusNode = focusNode.left;
+                    // if focusNode is null than it makes a child of the left node
                     if (focusNode == null) {
                         parent.left = newNode;
                         return;
                     }
+                    //if key is more than focusNode it goes to right
                 } else {
                     focusNode = focusNode.right;
+                    // if focusNode is null than it makes a child of the right node
                     if (focusNode == null) {
                         parent.right = newNode;
                         return;
@@ -37,30 +44,53 @@ public class BST <K extends Comparable<K>, V>{
 
     }
 
-    public V get(K key){
+    public V get(K key) {
+        //creating new node and giving the value of the root
+        Node currentNode = root;
+        //starting the loop until the node is zero
+        while (currentNode != null) {
+            // the result of compares with the current node is stored in result
 
+            int result = key.compareTo(currentNode.key);
+            //if in comparing it's less then 0 it goes to left,if more to right(larger)
+            if (result < 0) {
+                currentNode = currentNode.left;
+            }
+            else if (result > 0) {
+                currentNode = currentNode.right;
+            }
+            //when its 0 it means that we found our key
+            else {
+                return currentNode.val;
+            }
+        }
+        return null;
     }
+    //delete method
     public void delete(K key) {
         Node focusNode = root;
         Node parent = root;
-        boolean isLeftChild = true;
-
+        boolean isLeftChild = true; //assumption that focus node is a left child initially
+        //traversing until focus node is null and its key is not equal to the given key
         while (focusNode != null && focusNode.key.compareTo(key) != 0) {
             parent = focusNode;
+            //comparing key with the focus node
             if (key.compareTo(focusNode.key) < 0) {
                 focusNode = focusNode.left;
+                //left child have to be updated
                 isLeftChild = true;
             } else {
+                //right child have to be updated
                 focusNode = focusNode.right;
                 isLeftChild = false;
             }
         }
-
+        //nothing found
         if (focusNode == null) {
             return;
         }
 
-
+        // it checks if the node have to be deleted have no children
         if (focusNode.left == null && focusNode.right == null) {
             if (focusNode == root) {
                 root = null;
@@ -70,7 +100,7 @@ public class BST <K extends Comparable<K>, V>{
                 parent.right = null;
             }
         }
-
+        // if the node to be deleted has only one child, which is the right child.
         else if (focusNode.left == null) {
             if (focusNode == root) {
                 root = focusNode.right;
@@ -79,6 +109,7 @@ public class BST <K extends Comparable<K>, V>{
             } else {
                 parent.right = focusNode.right;
             }
+            // if the node to be deleted has only one child, which is the left child.
         } else if (focusNode.right == null) {
             if (focusNode == root) {
                 root = focusNode.left;
@@ -90,6 +121,8 @@ public class BST <K extends Comparable<K>, V>{
         }
 
         else {
+            //This block is executed if the node to be deleted has two children.
+            // In this case, a replacement node is found to take the deleted node's place.
             Node replacement = getReplacementNode(focusNode);
             if (focusNode == root) {
                 root = replacement;
